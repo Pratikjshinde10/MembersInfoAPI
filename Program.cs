402de587a58d87738188e403aa8aa01a39babb92
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2023-01-22 01:04:40 +0530

    Swagger Auth Changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,34 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( c => 
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "MembersInfoApi", Version = "v1" });
+    c.AddSecurityDefinition("oauth2" , new OpenApiSecurityScheme 
+    {
+        Description = "Oauth2.0 which uses authorizationcode flow",
+        Name = "oauth2.0",
+        Type=SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri(builder.Configuration["SwaggerAzureAD:AuthorizationUrl"]),
+                TokenUrl = new Uri(builder.Configuration["SwaggerAzureAD:TokenUrl"]),
+                Scopes = new Dictionary<string, string>
+                {
+                    { builder.Configuration["SwaggerAzureAD:Scope"],"Access API as user" }
+                }
+            }
+        }
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference{Type=ReferenceType.SecurityScheme, Id="oauth2"}
+            },
+            new[] {builder.Configuration["SwaggerAzureAD:Scope"] }
+        }
+    });
 });
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(builder.Configuration);
 
@@ -21,7 +50,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.OAuthClientId(builder.Configuration["SwaggerAzureAD:ClientId"]);
+        c.OAuthUsePkce();
+        c.OAuthScopeSeparator(" ");
+    });
 }
 
 app.UseHttpsRedirection();
