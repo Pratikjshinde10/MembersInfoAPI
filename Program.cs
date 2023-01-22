125b146c3bcb1512775d847ccdb9f2982af07370
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2023-01-22 10:31:39 +0530

    swagger doc

--- Program.cs
+++ Program.cs
@@ -47,17 +47,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMi
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c => 
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => 
-    {
-        c.OAuthClientId(builder.Configuration["SwaggerAzureAD:ClientId"]);
-        c.OAuthUsePkce();
-        c.OAuthScopeSeparator(" ");
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SwaggerAPI v1");
-    });
-}
+    c.OAuthClientId(builder.Configuration["SwaggerAzureAD:ClientId"]);
+    c.OAuthUsePkce();
+    c.OAuthScopeSeparator(" ");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SwaggerAPI v1");
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
